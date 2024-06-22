965debcf5b6760b4b14ae529e2d6f3fc175a6539
Edson Torres <66502687+EdsonTorres14@users.noreply.github.com>
2024-06-21 19:19:59 -0600

    153. Mostrando las criptomonedas en el formulario

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ const monedas = ref([
 ])
 
 const criptomonedas = ref([])
+
 onMounted(() => {
   const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
   fetch(url)
@@ -26,9 +27,20 @@ onMounted(() => {
           <label for="moneda">Moneda:</label>
           <select id="moneda">
             <option value="">-- Selecciona --</option>
-            <option v-for="moneda in monedas" :value="moneda.codigo"> {{ moneda.texto }}</option>
+            <option v-for="moneda in monedas" :value="moneda.codigo">{{ moneda.texto }}</option>
           </select>
         </div>
+
+        <div class="campo">
+          <label for="cripto">Criptomoneda:</label>
+          <select id="cripto">
+            <option value="">-- Selecciona --</option>
+            <option v-for="criptomoneda in criptomonedas" :value="criptomoneda.CoinInfo.Name">{{
+              criptomoneda.CoinInfo.FullName }}</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Cotizar">
       </form>
     </div>
   </div>
