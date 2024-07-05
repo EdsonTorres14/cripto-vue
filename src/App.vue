6f1460f6c1b49e1db5801e7b07d2da392de2db3a
Edson Torres <66502687+EdsonTorres14@users.noreply.github.com>
2024-07-04 18:49:22 -0600

    163. Limpiando nuestro template con componentes y composables

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 import { ref, reactive } from 'vue'
 import Alerta from './components/Alerta.vue'
 import Spinner from './components/Spinner.vue'
-import useCripto from './composables/useCripto';
+import Cotizacion from './components/Cotizacion.vue'
+import useCripto from './composables/useCripto'
 
-const { monedas, criptomonedas, cargando, cotizacion, obtenerCotizacion, mostrarResultados } = useCripto()
+const { monedas, criptomonedas, cotizacion, cargando, obtenerCotizacion, mostrarResultados } = useCripto()
 
 const error = ref('')
 
@@ -52,21 +53,8 @@ const cotizarCripto = () => {
       </form>
 
       <Spinner v-if="cargando" />
+      <Cotizacion v-if="mostrarResultados" :cotizacion="cotizacion" />
 
-      <div v-if="mostrarResultados" class="contenedor-resultado">
-        <h2>Cotización</h2>
-
-        <div class="resultado">
-          <img :src="'https://cryptocompare.com/' + cotizacion.IMAGEURL" alt="imagen cripto">
-          <div>
-            <p>El precio es de: <span>{{ cotizacion.PRICE }}</span></p>
-            <p>El precio más alto del día: <span>{{ cotizacion.HIGHDAY }}</span></p>
-            <p>El precio más bajo del día: <span>{{ cotizacion.LOWDAY }}</span></p>
-            <p>Variación últimas 24 horas: <span>{{ cotizacion.CHANGEPCT24HOUR }}</span></p>
-            <p>Última actualización: <span>{{ cotizacion.LASTUPDATE }}</span></p>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
